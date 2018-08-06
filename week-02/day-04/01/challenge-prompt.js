7de5760b999ded6029583984b04026aa9d9df61d
James Harden <sajeel23@berkeley.edu>
2018-08-05 23:56:29 -0700

    update day 4

--- week-02/day-04/01/challenge-prompt.js
+++ week-02/day-04/01/challenge-prompt.js
@@ -7,105 +7,158 @@ $(document).ready(function () {
     // write a onclick listener of .letterA 
     
         // append the letter A on the class of text-div
-        
+    $(".letterA").on("click", function(){
+        $("#text-div").append("A"); 
+    });
+
     // write a onclick listener of .letterB 
     
         // append the letter B on the class of text-div
-        
+    $(".letterB").on("click", function(){
+        $("#text-div").append("B"); 
+    });
     // write a onclick listener of .letterC
     
         // append the letter C on the class of text-div
-        
+    $(".letterC").on("click", function(){
+        $("#text-div").append("C"); 
+    });        
     // write a onclick listener of .letterD 
         
         // append the letter D on the class of text-div
-        
+    $(".letterC").on("click", function(){
+        $("#text-div").append("D"); 
+    });        
     // write a onclick listener of .letterE 
         
         // append the letter E on the class of text-div
-        
+    $(".letterE").on("click", function(){
+        $("#text-div").append("E"); 
+    });       
     // write a onclick listener of .letterF 
         
         // append the letter F on the class of text-div
-        
+    $(".letterF").on("click", function(){
+        $("#text-div").append("F"); 
+    });        
     // write a onclick listener of .letterG 
         
         // append the letter G on the class of text-div
-        
+    $(".letterG").on("click", function(){
+        $("#text-div").append("G"); 
+    });         
     // write a onclick listener of .letterH 
         
         // append the letter H on the class of text-div
-        
+    $(".letterH").on("click", function(){
+        $("#text-div").append("H"); 
+    });           
     // write a onclick listener of .letterI 
         
         // append the letter I on the class of text-div
-        
+     $(".letterI").on("click", function(){
+        $("#text-div").append("I"); 
+    });          
     // write a onclick listener of .letterJ 
         
         // append the letter J on the class of text-div
-        
+    $(".letterJ").on("click", function(){
+        $("#text-div").append("J"); 
+    });       
     // write a onclick listener of .letterK 
         
         // append the letter K on the class of text-div
-        
+    $(".letterK").on("click", function(){
+        $("#text-div").append("K"); 
+    });           
     // write a onclick listener of .letterL 
         
         // append the letter L on the class of text-div
-        
+    $(".letterL").on("click", function(){
+        $("#text-div").append("L"); 
+    });           
     // write a onclick listener of .letterM 
         
         // append the letter M on the class of text-div
-        
+    $(".letterM").on("click", function(){
+        $("#text-div").append("M"); 
+    });           
     // write a onclick listener of .letterN 
         
         // append the letter N on the class of text-div
-        
+    $(".letterN").on("click", function(){
+        $("#text-div").append("N"); 
+    });           
     // write a onclick listener of .letterO 
         
         // append the letter O on the class of text-div
-        
+    $(".letterO").on("click", function(){
+        $("#text-div").append("O"); 
+    });           
     // write a onclick listener of .letterP 
         
         // append the letter P on the class of text-div
-        
+    $(".letterP").on("click", function(){
+        $("#text-div").append("P"); 
+    });           
     // write a onclick listener of .letterQ 
         
         // append the letter Q on the class of text-div
-        
+     $(".letterQ").on("click", function(){
+        $("#text-div").append("Q"); 
+    });          
     // write a onclick listener of .letterR 
         
         // append the letter R on the class of text-div
-        
+    $(".letterR").on("click", function(){
+        $("#text-div").append("R"); 
+    });           
     // write a onclick listener of .letterS 
         
         // append the letter S on the class of text-div
-        
+    $(".letterS").on("click", function(){
+        $("#text-div").append("S"); 
+    });           
     // write a onclick listener of .letterT 
         
         // append the letter T on the class of text-div
-        
+    $(".letterT").on("click", function(){
+        $("#text-div").append("T"); 
+    });           
     // write a onclick listener of .letterU 
         
         // append the letter U on the class of text-div
-        
+    $(".letterU").on("click", function(){
+        $("#text-div").append("U"); 
+    });           
     // write a onclick listener of .letterV 
         
         // append the letter V on the class of text-div
-        
+    $(".letterV").on("click", function(){
+        $("#text-div").append("V"); 
+    });           
     // write a onclick listener of .letterW 
         
         // append the letter W on the class of text-div
-        
+    $(".letterW").on("click", function(){
+        $("#text-div").append("W"); 
+    });           
     // write a onclick listener of .letterX 
         
         // append the letter X on the class of text-div
-        
+    $(".letterX").on("click", function(){
+        $("#text-div").append("X"); 
+    });           
     // write a onclick listener of .letterY 
         
         // append the letter Y on the class of text-div
-        
+    $(".letterY").on("click", function(){
+        $("#text-div").append("Y"); 
+    });           
     // write a onclick listener of .letterZ 
         
         // append the letter Z on the class of text-div
-        
+    $(".letterZ").on("click", function(){
+        $("#text-div").append("Z"); 
+    });          
 });
--- week-02/day-04/02/challenge-prompt.js
+++ week-02/day-04/02/challenge-prompt.js
@@ -4,20 +4,33 @@ $(document).ready(function () {
 
         //1. create a variable called text and have it equal an empty string
 
+        var text = "";
+
         //2. create a function called randomizingText
+
+        function randomizingText(){
 
             //3. create a variable called possibleLetters set to a string containing the whole alphabet. For example possibleLetters = "ABCD..."
 
+            var possibleLetters = "abcdefghijklmnopqrstuvwxyz";
             // 4. The text variable will continue to grow in length as the user clicks the question mark. Reassign the text variable to itself plus a new random letter. You will need to use the possibleLetters variable to accomplish this. Research the charAt() method if you're stuck.
+            var randomIndex = Math.floor(Math.random()*possibleLetters.length);
+
+            text = text + possibleLetters[randomIndex];
 
             //5. Finally have this function return the current value of the text variable
+            return text;
+        }
+    
+    
 
     $(".jumbotron").on("click", "#randomLetter", function () {
 
-
+        randomizingText();
         // console.log the function randomizingText (don't forget to invoke the function)
-
+        console.log(randomizingText())
         // append the text variable to the h1 tag with the id of letters
+        $("#letters").append(text);
 
 
     });
